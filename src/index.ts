2c8a3ae43457e59d6c74ce5a982d141d42296d3e
aditya yadav <dedomil@tuta.io>
2025-04-18 18:49:33 +0530

    fix 400 error

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = new Hono<{ Bindings: Env }>();
 
 app.use(async (c, next) => {
 	const authHeader = c.req.header('authorization');
-	if (authHeader !== c.env.WEBHOOK_TOKEN) return c.status(400);
+	if (authHeader !== c.env.WEBHOOK_TOKEN) return c.json([], 400);
 	await next();
 });
 
@@ -20,7 +20,7 @@ app.post('/', async (c) => {
 
 	const message = payload.map((e) => `${e.description}\n\nhttps://solscan.io/tx/${e.signature}`).join('\n\n');
 	await bot.api.sendMessage(5278367192, message, { link_preview_options: { is_disabled: true } });
-	return c.status(200);
+	return c.json([], 200);
 });
 
 export default app;
